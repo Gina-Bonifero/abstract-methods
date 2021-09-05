095b75dde62c3522a3378adb00b56e34d21ed9d2
Gina-Bonifero <gvbonifero@gmail.com>
2021-09-05 17:10:57 -0300

    melhorias na indentação

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,16 +17,22 @@ public class Program {
 		List<Pessoa> list = new ArrayList<>();
 		
 		System.out.print("Enter the number of tax payers: ");
+		
 		int n = sc.nextInt();
+		
 		for (int i = 1; i <= n; i++) {
 			System.out.println("Tax payer #" + i + " data:");
+			
 			System.out.print("Individual or company (i/c)? ");
 			char c = sc.next().charAt(0);
 			sc.nextLine();
+			
 			System.out.print("Name: ");
 			String name = sc.nextLine();
+			
 			System.out.print("Anual income: ");
 			Double rendaAnual = sc.nextDouble();
+			
 			if (c == 'i') {
 				System.out.print("Health expenditures: ");
 				double gastosSaude = sc.nextDouble();
